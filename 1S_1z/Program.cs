4ea6affdc4c33977967ed7e49190006b5a866879
Siberprost <Skripnichencko.nikita@yandex.ru>
2022-07-08 20:29:18 +0300

    1S_1z

---
+++ 1S_1z/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число 1: ");
+string number_1 = Console.ReadLine();
+Console.Write("Введите число 2: ");
+string number_2 = Console.ReadLine();
+
+
+if(number_1 == number_1 * number_1)
+{
+    Console.WriteLine("1 число является квадратным корнем второго");
+}
+else
+{
+    Console.WriteLine("1 число не является квадратным корнем второго");
+
+}
