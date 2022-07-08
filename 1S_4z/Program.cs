356312caf55cef1ea9fdd1a9ea039dbcf4b40fa3
Siberprost <Skripnichencko.nikita@yandex.ru>
2022-07-08 22:03:53 +0300

    1S_4z-made Void

--- 1S_4z/Program.cs
+++ 1S_4z/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//7. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+void Zadacha7()
+{
+    Console.Write("Введите число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    int start = -num;
+    while (start<= num)
+    {
+        Console.Write(start + " ");
+        start++;
+    }
+    Console.WriteLine();
+}
+Zadacha7();
