ac4bba7888e7c9e096338a2b19db01293284469b
Siberprost <Skripnichencko.nikita@yandex.ru>
2022-07-08 20:17:31 +0300

    added 1lessonSeminar

---
+++ 9_kwadrat_chisla/Program.cs
@@ -0,0 +1,9 @@
+﻿// тип_данных имя_переменных = занчение;
+// тип_данных имя_переменной;
+int number = 5;
+int result;
+
+result = number * number;
+Console.WriteLine(result);
+//consile-обращение, . разделение,  WriteLine указание на действие, ()- то, что хотим видеть.;
+
