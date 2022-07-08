abbda3908d41315ce33acfd82a90cf2692f92aec
Siberprost <Skripnichencko.nikita@yandex.ru>
2022-07-08 21:31:37 +0300

    1S_3z-made function

---
+++ 1S_3z/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу вычисления значения 
+//функции возведения числа в квадрат
+
+//функция должна возвращать значение для этого return
+
+int Power(int currentNumber)
+{
+    return currentNumber * currentNumber;
+}
+// в строках 6-7 мы сделали СВОЮ функцию и назвали ее POWER и она выполняет задачу.
+
+int number = 5;
+int result = Power(number);
+Console.WriteLine(result);
