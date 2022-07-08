bce0973791772e5d79f4754c395d97899dd55e56
Siberprost <Skripnichencko.nikita@yandex.ru>
2022-07-08 20:46:55 +0300

    1S_1z_end

--- 1S_1z/Program.cs
+++ 1S_1z/Program.cs
@@ -1,8 +1,7 @@
 ﻿Console.Write("Введите число 1: ");
-string number_1 = Console.ReadLine();
+int number_1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число 2: ");
-string number_2 = Console.ReadLine();
-
+int number_2 = Convert.ToInt32(Console.ReadLine());
 
 if(number_2 == number_1 * number_1)
 {
