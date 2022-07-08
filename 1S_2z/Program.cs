9683722335b517e7e8f97426b342bb00e3a35466
Siberprost <Skripnichencko.nikita@yandex.ru>
2022-07-08 20:55:43 +0300

    1S_2z_2

--- 1S_2z/Program.cs
+++ 1S_2z/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число дня недели: ");
+int number_week = Convert.ToInt32(Console.ReadLine());
+
+if(number_week == 3)
+{
+    Console.WriteLine("Среда, пить рано!");
+}
+if(number_week == 5)
+{
+    Console.WriteLine("Пятница, сегодя вечером нажрусь!");
+}
+else
+{
+    Console.WriteLine("Указано некорректное число недели");
+
+}
