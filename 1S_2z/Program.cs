83ba21de8062c7a63dddc219171e0d02a84c6210
Siberprost <Skripnichencko.nikita@yandex.ru>
2022-07-08 21:10:22 +0300

    1S_2z_2end_7days

--- 1S_2z/Program.cs
+++ 1S_2z/Program.cs
@@ -1,13 +1,33 @@
 ﻿Console.Write("Введите число дня недели: ");
 int number_week = Convert.ToInt32(Console.ReadLine());
 
-if(number_week == 3)
+if(number_week == 1)
+{
+    Console.WriteLine("Понедельник, земля гудроном!");
+}
+else if(number_week == 2)
+{
+    Console.WriteLine("Вторник, разгоняйся!");
+}
+else if(number_week == 3)
 {
     Console.WriteLine("Среда, пить рано!");
+}
+else if(number_week == 4)
+{
+    Console.WriteLine("Четверг, Ещё чуть-чуть!");
 }
 else if(number_week == 5)
 {
     Console.WriteLine("Пятница, сегодя вечером нажрусь!");
+}
+else if(number_week == 6)
+{
+    Console.WriteLine("Суббота, болеешь?");
+}
+else if(number_week == 7)
+{
+    Console.WriteLine("Воскресенье, живи, завтра на работу!");
 }
 else
 {
