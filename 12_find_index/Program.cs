64ffa9ebe407c961d10a6e7d5cb3ce3677d35869
Siberprost <Skripnichencko.nikita@yandex.ru>
2022-07-12 15:43:01 +0300

    added_12_find_index_done

---
+++ 12_find_index/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] array = { 1, 12, 31, 18, 4, 15, 16, 17, 18 };
+
+int n = array.Length;
+// array.Lenght возвращает нам инфу о длинне массива
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+        // break - оператор прерывания после нахождеения первого искомого значения. Если в массиве несколько одинаковых искомых значений, то без break нам покажут все, с break первый.
+    }
+    index++;
+    //index = index + 1;
+}
